4b74f4337fdbb062606a6d1ce77986735706a88c
pratikshasarukte <157965887+pratikshasarukte@users.noreply.github.com>
2024-10-12 15:40:19 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDh_5z1kO_oJqp32igX6D-RGGJRNJ7pmWY",
+    authDomain: "edulift-3aadf.firebaseapp.com",
+    projectId: "edulift-3aadf",
+    storageBucket: "edulift-3aadf.appspot.com",
+    messagingSenderId: "270870867730",
+    appId: "1:270870867730:web:5f1bbd0e59d93c3aeecdcb",
+    measurementId: "G-3Y98MQXHTM"
+  };
+
+firebase.initializeApp(firebaseConfig);
